1724b5f3b410b8e9745b0115b8a21c4aa37b5b6a
Emiliano R Gillini <emigillini@hotmail.com>
2023-05-08 09:18:47 -0300

    entregable

--- src/App.js
+++ src/App.js
@@ -1,36 +1,55 @@
-import express from 'express'
-import fs from 'fs'
+import express from "express";
+import fs from "fs/promises";
 
 const path = "productos.json";
 const utf = "utf-8";
 
-const app=express()
-app.use(express.urlencoded({extended:true}))
+const app = express();
+app.use(express.urlencoded({ extended: true }));
 
-
-app.get('/products', (req, res) => {
-
-    const data = fs.readFileSync(path, utf);
+app.get("/", async (req, res) => {
+  try {
+    const data = await fs.readFile(path, utf);
     const products = JSON.parse(data);
-    const limit = parseInt(req.query.limit); 
-
-    if (limit) {
-    const firstProducts = products.slice(0, limit);
-    res.send(firstProducts);
-    } else {
     res.send(products);
-    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error interno del servidor");
+  }
 });
 
-app.get('/products/:id', (req, res) => {
-    const data = fs.readFileSync(path, utf);
+app.get("/products", async (req, res) => {
+  try {
+    const data = await fs.readFile(path, utf);
     const products = JSON.parse(data);
-    let id= req.params.id
-    let prod = products.find((prod)=>{return prod.id === parseInt(id)})
-    if(!prod){return res.send(`{id${id} :"id no encontrado"}`)}
-    res.send(prod)
-})
+    const limit = parseInt(req.query.limit);
 
+    if (limit) {
+      const firstProducts = products.slice(0, limit);
+      res.send(firstProducts);
+    } else {
+      res.send(products);
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error interno del servidor");
+  }
+});
 
+app.get("/products/:id", async (req, res) => {
+  try {
+    const data = await fs.readFile(path, utf);
+    const products = JSON.parse(data);
+    const id = req.params.id;
+    const prod = products.find((prod) => prod.id === parseInt(id));
+    if (!prod) {
+      return res.send({ id: id, message: `id ${id} no encontrado ` });
+    }
+    res.send(prod);
+  } catch (error) {
+    console.error(error);
+    res.status(404).send("Error id no encontrado");
+  }
+});
 
-const server= app.listen(8080, ()=> console.log("conectado "))
+const server = app.listen(8080, () => console.log("conectado"));
--- src/entregable.js
+++ src/entregable.js
@@ -1,5 +1,4 @@
-
-import fs from 'fs'
+import fs from "fs";
 const path = "productos.json";
 const utf = "utf-8";
 
@@ -11,96 +10,205 @@ class ProductManager {
     this.path = path;
   }
 
-  addProduct(title, description, price, thumbnail, code, stock) {
+  async addProduct(title, description, price, thumbnail, code, stock) {
     ProductManager.id++;
-    const data = fs.readFileSync(path, utf);
-    const products = JSON.parse(data);
-    const productExists = products.some((p) => p.code === code);
-    if (productExists) {
-      console.error(`Error: El código ${code} ya existe.`);
-      return;
-    }
-    const product = {
-      title,
-      description,
-      price,
-      thumbnail,
-      code,
-      stock,
-      id: ProductManager.id,
-    };
-    products.push(product);
-    fs.writeFileSync(path, JSON.stringify(products));
-    console.log(`Se agregó el producto "${title}" al archivo ${path}.`);
-  }
-
-  getProducts() {
-    let mostarProd = fs.readFileSync(path, utf);
-    let productos = JSON.parse(mostarProd);
-    console.log(productos);
-    return productos;
-  }
-
-  getProductById(id) {
-    const data = fs.readFileSync(path, utf);
-    const product = JSON.parse(data);
-    const products = product.find((p) => p.id === id);
-    if (products) {
-      console.log("Este es su producto:", products);
-      return products;
-    } else {
-      console.error(`Error: Producto con id ${id} no encontrado.`);
-    }
-  }
-
-  deleteProd(id) {
-    const data = fs.readFileSync(path, utf);
-    const product = JSON.parse(data);
-    let indexOf = product.findIndex((p) => p.id === id);
-    product.splice(indexOf, 1);
-    fs.writeFileSync(path, JSON.stringify(product));
-    console.log(`Se eliminó el producto con id ${id}.`);
-  }
-
-  updateProduct(id, title, description, price, thumbnail, code, stock) {
-    const data = fs.readFileSync(path, utf);
-    const products = JSON.parse(data);
-    let product = products.find((p) => p.id === id);
-    if (product) {
-      const prodUpd = {
-        title: title || product.title,
-        description: description || product.description,
-        price: price || product.price,
-        thumbnail: thumbnail || product.thumbnail,
-        code: code || product.code,
-        stock: stock || product.stock,
-        id: id,
+    try {
+      const data = await fs.promises.readFile(path, utf);
+      const products = JSON.parse(data);
+      const productExists = products.some((p) => p.code === code);
+      if (productExists) {
+        console.error(`Error: El código ${code} ya existe.`);
+        return;
+      }
+      const product = {
+        title,
+        description,
+        price,
+        thumbnail,
+        code,
+        stock,
+        id: ProductManager.id,
       };
-      let indexOf = products.findIndex((p) => p.id === id);
-      products[indexOf] = prodUpd;
-      fs.writeFileSync(path, JSON.stringify(products));
-      console.log(`Se actualizó ${prodUpd.title}`);
-    } else {
-      console.error(`Error: Producto con id ${id} no encontrado.`);
+      products.push(product);
+      await fs.promises.writeFile(path, JSON.stringify(products));
+      console.log(`Se agregó el producto "${title}" al archivo ${path}.`);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send("Error interno del servidor");
+    }
+  }
+
+  async getProducts() {
+    try {
+      let mostarProd = await fs.promises.readFile(path, utf);
+      let productos = JSON.parse(mostarProd);
+      console.log(productos);
+      return productos;
+    } catch (error) {
+      console.error(error);
+      res.status(500).send("Error interno del servidor");
+    }
+  }
+
+  async getProductById(id) {
+    try {
+      const data = await fs.promises.readFile(path, utf);
+      const product = JSON.parse(data);
+      const products = product.find((p) => p.id === id);
+      if (products) {
+        console.log("Este es su producto:", products);
+        return products;
+      } else {
+        console.error(`Error: Producto con id ${id} no encontrado.`);
+      }
+    } catch (error) {
+      console.error(error);
+      res.status(500).send("Error interno del servidor");
+    }
+  }
+
+  async deleteProd(id) {
+    try {
+      const data = await fs.promises.readFile(path, utf);
+      const product = JSON.parse(data);
+      let indexOf = product.findIndex((p) => p.id === id);
+      product.splice(indexOf, 1);
+      await fs.promises.writeFile(path, JSON.stringify(product));
+      console.log(`Se eliminó el producto con id ${id}.`);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send("Error interno del servidor");
+    }
+  }
+
+  async updateProduct(id, title, description, price, thumbnail, code, stock) {
+    try {
+      const data = await fs.promises.readFile(path, utf);
+      const products = JSON.parse(data);
+      let product = products.find((p) => p.id === id);
+      if (product) {
+        const prodUpd = {
+          title: title || product.title,
+          description: description || product.description,
+          price: price || product.price,
+          thumbnail: thumbnail || product.thumbnail,
+          code: code || product.code,
+          stock: stock || product.stock,
+          id: id,
+        };
+        let indexOf = products.findIndex((p) => p.id === id);
+        products[indexOf] = prodUpd;
+        await fs.promises.writeFile(path, JSON.stringify(products));
+        console.log(`Se actualizó ${prodUpd.title}`);
+      } else {
+        console.error(`Error: Producto con id ${id} no encontrado.`);
+      }
+    } catch (error) {
+      console.error(error);
+      res.status(500).send("Error interno del servidor");
     }
   }
 }
 
 const productManager1 = new ProductManager();
 
-productManager1.addProduct("lechuga", "muy fresca", 100, "www.imagen", 1, 1000);
-productManager1.addProduct("tomate", "muy fresca", 100, "www.imagen", 2, 1000);
-productManager1.addProduct("pepino", "muy fresca", 1000, "www.imagen", 3, 1000);
-productManager1.addProduct("banana", "muy fresca", 500, "www.imagen", 4, 1000);
-productManager1.addProduct("manzana", "muy fresca", 500, "www.imagen", 5, 1000);
-productManager1.addProduct("apio", "muy fresca", 500, "www.imagen", 6, 1000);
-productManager1.addProduct("pera", "muy fresca", 500, "www.imagen", 7, 1000);
-productManager1.addProduct("cebolla", "muy fresca", 500, "www.imagen", 8, 1000);
-productManager1.addProduct("remolacha", "muy fresca", 500, "www.imagen", 9, 1000);
-productManager1.addProduct("naranja", "muy fresca", 500, "www.imagen", 10, 1000);
-productManager1.addProduct("mandarina", "muy fresca", 500, "www.imagen", 11, 1000);
-productManager1.addProduct("anana", "muy fresca", 500, "www.imagen", 12, 1000);
-productManager1.updateProduct(
+await productManager1.addProduct(
+  "lechuga",
+  "muy fresca",
+  100,
+  "www.imagen",
+  1,
+  1000
+);
+await productManager1.addProduct(
+  "pepino",
+  "muy fresca",
+  1000,
+  "www.imagen",
+  3,
+  1000
+);
+await productManager1.addProduct(
+  "banana",
+  "muy fresca",
+  500,
+  "www.imagen",
+  4,
+  1000
+);
+await productManager1.addProduct(
+  "manzana",
+  "muy fresca",
+  500,
+  "www.imagen",
+  5,
+  1000
+);
+await productManager1.addProduct(
+  "tomate",
+  "muy fresca",
+  100,
+  "www.imagen",
+  2,
+  1000
+);
+await productManager1.addProduct(
+  "apio",
+  "muy fresca",
+  500,
+  "www.imagen",
+  6,
+  1000
+);
+await productManager1.addProduct(
+  "pera",
+  "muy fresca",
+  500,
+  "www.imagen",
+  7,
+  1000
+);
+await productManager1.addProduct(
+  "cebolla",
+  "muy fresca",
+  500,
+  "www.imagen",
+  8,
+  1000
+);
+await productManager1.addProduct(
+  "remolacha",
+  "muy fresca",
+  500,
+  "www.imagen",
+  9,
+  1000
+);
+await productManager1.addProduct(
+  "naranja",
+  "muy fresca",
+  500,
+  "www.imagen",
+  10,
+  1000
+);
+await productManager1.addProduct(
+  "mandarina",
+  "muy fresca",
+  500,
+  "www.imagen",
+  11,
+  1000
+);
+await productManager1.addProduct(
+  "anana",
+  "muy fresca",
+  500,
+  "www.imagen",
+  12,
+  1000
+);
+await productManager1.updateProduct(
   3,
   "papa",
   "muy rica",
@@ -109,7 +217,7 @@ productManager1.updateProduct(
   3,
   10000
 );
-productManager1.updateProduct(
+await productManager1.updateProduct(
   3,
   "taca",
   undefined,
@@ -118,6 +226,6 @@ productManager1.updateProduct(
   undefined,
   undefined
 );
-productManager1.getProductById(3);
-productManager1.deleteProd(1);
-productManager1.deleteProd(2);
+await productManager1.getProductById(3);
+await productManager1.deleteProd(1);
+await productManager1.deleteProd(2);
